1a3e2aca404c5aade8771ad43a6d99186f474ad8
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-29 22:46:32 +0530

    Intial setup

---
+++ ProductstList/ProductstListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductstListApp.swift
+//  ProductstList
+//
+//  Created by Keerthi Devipriya(kdp) on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductstListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

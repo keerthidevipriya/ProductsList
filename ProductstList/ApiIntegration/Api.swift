16547e65b8430e131a680f6ed4439f45df6f8b97
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-30 01:08:55 +0530

    ADded logic for album group in the viewmodel

--- ProductstList/ApiIntegration/Api.swift
+++ ProductstList/ApiIntegration/Api.swift
@@ -28,6 +28,34 @@ class ApiIntegration {
     }
 }
 
-class AlbumVC {
-    
+class AlbumsViewModel {
+    var model = ApiIntegration().loadJson()
+    func getAlbumGroup() -> [Int: [Album]] {
+        let albumgroup = model.group(by: {$0.albumId})
+        return albumgroup
+    }
+}
+
+class Box<A> {
+    var value: A
+    init(_ val: A) {
+        self.value = val
+    }
+}
+
+public extension Sequence {
+    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U: [Iterator.Element]] {
+        var categories: [U: Box<[Iterator.Element]>] = [:]
+        for element in self {
+            let key = key(element)
+            if case nil = categories[key]?.value.append(element) {
+                categories[key] = Box([element])
+            }
+        }
+        var result: [U: [Iterator.Element]] = Dictionary(minimumCapacity: categories.count)
+        for (key, val) in categories {
+            result[key] = val.value
+        }
+        return result
+    }
 }

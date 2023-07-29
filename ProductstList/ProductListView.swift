2501b3dd29a4715fd2866ef1d2964b074e5fcbc3
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-29 23:15:36 +0530

    added the navigation item

--- ProductstList/ProductListView.swift
+++ ProductstList/ProductListView.swift
@@ -9,27 +9,29 @@ import SwiftUI
 
 struct ProductListView: View {
     var body: some View {
-        List(0..<20) { item in
-            Image("img1")
-                .resizable()
-                .scaledToFit()
-                .imageScale(.large)
-                .frame(height: 90)
-                .cornerRadius(4)
-                .foregroundColor(.accentColor)
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Hi this is keerthi devipriya sharing my views")
-                    .lineLimit(2)
-                    //.fontWeight(.semibold)
-                    .minimumScaleFactor(0.5)
-                Text("21 oct 2021")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-                
+        NavigationView {
+            List(0..<20) { item in
+                Image("img1")
+                    .resizable()
+                    .scaledToFit()
+                    .imageScale(.large)
+                    .frame(height: 90)
+                    .cornerRadius(4)
+                    .foregroundColor(.accentColor)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Hi this is keerthi devipriya sharing my views")
+                        .lineLimit(2)
+                        //.fontWeight(.semibold)
+                        .minimumScaleFactor(0.5)
+                    Text("21 oct 2021")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    
+                }
+                //.padding()
             }
             //.padding()
         }
-        //.padding()
     }
 }
 

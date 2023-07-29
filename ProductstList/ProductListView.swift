5fd6c6fd7d34432e17710f5b89097beb9871ac48
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-30 00:44:46 +0530

    Created a sections view in list tablle

--- ProductstList/ProductListView.swift
+++ ProductstList/ProductListView.swift
@@ -12,22 +12,27 @@ struct ProductListView: View {
     var body: some View {
         NavigationView {
             List(0..<albums.count) { item in
-                Image("img1")
-                    .resizable()
-                    .scaledToFit()
-                    .imageScale(.large)
-                    .frame(height: 90)
-                    .cornerRadius(4)
-                    .foregroundColor(.accentColor)
-                VStack(alignment: .leading, spacing: 5) {
-                    Text(albums[item].title ?? "Default Title")
-                        .lineLimit(2)
-                        //.fontWeight(.semibold)
-                        .minimumScaleFactor(0.5)
-                    Text("21 oct 2021")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                    
+                Section {
+                    Image("img1")
+                        .resizable()
+                        .scaledToFit()
+                        .imageScale(.large)
+                        .frame(height: 90)
+                        .cornerRadius(4)
+                        .foregroundColor(.accentColor)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(albums[item].title ?? "Default Title")
+                            .lineLimit(2)
+                            //.fontWeight(.semibold)
+                            .minimumScaleFactor(0.5)
+                        Text("21 oct 2021")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        
+                    }
+                    //.padding()
+                } header: {
+                    Text("Sections")
                 }
                 //.padding()
             }

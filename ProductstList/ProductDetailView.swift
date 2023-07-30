e2cf12754f70b007725ca7a119270a32e9808b9b
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-30 17:20:57 +0530

    showing the alert on click of ok button in detail page

--- ProductstList/ProductDetailView.swift
+++ ProductstList/ProductDetailView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ProductDetailView: View {
-    @State var showsAlert = false
+    @State private var isAlert = false
     var album: Album
     var body: some View {
         VStack(spacing: 20) {
@@ -35,13 +35,33 @@ struct ProductDetailView: View {
             
            Spacer()
             
-            Link(destination: URL(string: album.thumbnailUrl)!, label: {
+            /*Link(destination: URL(string: album.thumbnailUrl)!, label: {
                 StdBtn(title: "Save")
-            })
-        }.padding()
+            })*/
+            
+            Button(action: {
+                self.isAlert = true
+            }) {
+                Text("Update Title")
+                .foregroundColor(Color.white)
+            }
+            .bold()
+            .font(.subheadline)
+            .frame(width: 220,height: 30)
+            .background(Color(.red))
+            .foregroundColor(.white)
+            .cornerRadius(4)
+            .alert(isPresented: $isAlert) { () -> Alert in
+                Alert(title: Text("Update Title"), message: Text("Provide your updated title here"), primaryButton: .default(Text("Ok"), action: {
+                    updateTitle("new")
+                }), secondaryButton: .cancel())
+        }
+        }
+            
     }
     
     func updateTitle(_ title: String) {
+        print("\(title)----")
         //updateAlbum(album.id, title: "new")
     }
 }

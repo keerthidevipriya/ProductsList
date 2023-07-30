3446163e26b2264be69d7512b5b69b2b85dd8a7e
keerthidevipriya <keerthidevipriya@gmail.com>
2023-07-30 15:52:28 +0530

    Created a seperate product cell

--- ProductstList/ProductListView.swift
+++ ProductstList/ProductListView.swift
@@ -13,33 +13,42 @@ struct ProductListView: View {
         NavigationView {
             List(0..<albums.count) { item in
                 NavigationLink(destination: ProductDetailView(album: albums[item]), label: {
-                    //Section {
-                        Image("img1")
-                            .resizable()
-                            .scaledToFit()
-                            .imageScale(.large)
-                            .frame(height: 90)
-                            .cornerRadius(4)
-                            .foregroundColor(.accentColor)
-                            .padding(.vertical, 4)
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text(albums[item].title ?? "Default Title")
-                                .lineLimit(2)
-                                //.fontWeight(.semibold)
-                                .minimumScaleFactor(0.5)
-                            Text("title can b updated")
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                            
-                        }
-                        //.padding()
-                   /*} header: {
-                       Text("AlbumId: \(albums[item].albumId)")
-                   }*/
+                    ProductCell(album: albums[item])
                 })
-                //.padding()
             }
             .navigationTitle("List")
+        }
+    }
+}
+
+struct ProductCell: View {
+    var album : Album
+    
+    var body: some View {
+        HStack {
+            //Section {
+                Image("img1")
+                    .resizable()
+                    .scaledToFit()
+                    .imageScale(.large)
+                    .frame(height: 90)
+                    .cornerRadius(4)
+                    .foregroundColor(.accentColor)
+                    .padding(.vertical, 4)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(album.title ?? "Default Title")
+                        .lineLimit(2)
+                        //.fontWeight(.semibold)
+                        .minimumScaleFactor(0.5)
+                    Text("Title can b updated")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    
+                }
+                //.padding()
+           /*} header: {
+               Text("AlbumId: \(albums[item].albumId)")
+           }*/
         }
     }
 }
